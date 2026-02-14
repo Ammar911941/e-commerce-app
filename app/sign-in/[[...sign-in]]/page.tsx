21e0b7aaac98dedc9859c60a5e55515255fe39e4
Ammar Amr <ammaramr94736@gmail.com>
2026-02-14 17:40:02 +0200

    Update page.tsx

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
-    <div className="flex justify-center items-center py-8">
+    <div className="flex justify-center items-center py-8 mt-35">
       <SignIn />
     </div>
   );
